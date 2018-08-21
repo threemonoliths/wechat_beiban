fb7d0b3b0d6bdebb5ce5eef7253dbbf38cf5d904
jzw <you@example.com>
2018-08-21 15:29:36 +0800

    导航栏搜索栏取消预定

--- room_reservation_app/src/pages/contact/contact.ts
+++ room_reservation_app/src/pages/contact/contact.ts
@@ -3,35 +3,71 @@ import { NavController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import{ contactpages}from'./contactpage';
 import { ContactService } from './service';
+import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage implements OnInit {
-  pages:contactpages[];
-
+  
+  pages:contactpages[];//房间订单
   // cancelOrderInfo : any = null;
+  roomLayouts: any[] = [];//房型
 
-  q: any = {
+  q: any = {             //排序
     page_index: 1,
     page_size: 15,
     sort_field: "start_time",
     sort_direction: "asc"
   };
 
+  order: string = "rooms";//导航
+  isAndroid: boolean = false;
+  searchInput:number;
+
+  //public Status:any;//按钮
 
   constructor(public navCtrl: NavController,
     private contactService: ContactService,
     public alertCtrl: AlertController,
-  
-  ) {}
-
-  ngOnInit() {
+    platform: Platform,
+  ) {
+    this.isAndroid = platform.is('android');
     this.getPages();
   }
- 
-  getPages():void {
+
+   //搜索后的显示列表
+  isSearching=false;
+  searchingItems=[];
+  onCancelSearch(event){
+    this.isSearching=false;
+    this.searchingItems=[];
+  }
+  getItems(ev:any) {
+    console.log('getItems');
+    this.isSearching=true;
+    this.initializeItems();
+    let val = ev.target.value;
+    this.searchInput=val;
+    if (val && val.trim() != '') {
+        this.searchingItems = this.searchingItems.filter((i) => {
+        return (i.start_time.indexOf(val) > -1);
+      })
+    } else{
+      this.isSearching=false;
+    }
+  }
+  initializeItems(){
+    this.searchingItems = this.pages;
+  }
+
+  ngOnInit() {
+    this.getPages();  
+  }
+
+ //获取房间订单信息
+  getPages():void {   
     this.contactService.listOnePage(this.q)
       .then(resp => {
         if (resp.error) {
@@ -41,10 +77,10 @@ export class ContactPage implements OnInit {
           console.log(this.pages);
         }
       })
-      .catch((error) => {error => console.log(error)})
+      .catch((error) => {error => console.log(error)})  
   }
   
-
+//取消预定
 
   cancelOrder(i) {
     let alert = this.alertCtrl.create({
@@ -63,15 +99,25 @@ export class ContactPage implements OnInit {
             console.log('Cancle clicked');
             console.log(i)
             this.contactService.cancelOrder(i).then(resp => console.log(resp))
-
+            this.contactService.cancelOrder(i).then(resp => {    //显示取消成功
+              if (resp.data) { this.showOK() }
+            })
+           
+            
           }
-        }
+        },
       ]
+      }); 
+    alert.present();
+  }
+  
+  showOK() {
+    const alert = this.alertCtrl.create({
+      title: '取消成功',
+      subTitle: '您的订单已取消',
     });
     alert.present();
   }
   
-
-
-
+ 
 }
--- room_reservation_app/src/pages/contact/service.ts
+++ room_reservation_app/src/pages/contact/service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
 import{ contactpages} from './contactpage';
-// import{ pages} from './page';
-
 import { Http, URLSearchParams, RequestOptions, Headers, ResponseContentType } from '@angular/http';
 import { baseUrl, getTokenOptions } from '../../shared/shared';
-// import { Observable, of } from 'rxjs';
+
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
@@ -14,13 +12,13 @@ export class ContactService {
   // getPages() {
   //   return pages;
   // } 
-
   url = baseUrl + "room_order_info";
-
     listOnePage(q) {
         return this.http.get(this.url, getTokenOptions(q))
                    .toPromise().then(res => {return res.json()})           
     }
+
+
 
     cancelOrder(i) {
       let v = {room_order_info: null};
@@ -37,4 +35,6 @@ export class ContactService {
         .toPromise().then(res => {return res.json()})   
     }
 
+    
+    
 }
