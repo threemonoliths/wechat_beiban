b56571d716d8ab03f7cd664b9ed2eb70e743c3e9
tms <threemonoliths@gmail.com>
2018-08-21 15:42:34 +0800

    hb

--- room_reservation_app/src/pages/tabs/tabs.ts
+++ room_reservation_app/src/pages/tabs/tabs.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { PreviewPage } from '../preview/preview';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 import { CarPage } from '../car/car';
 
+import { TabService } from '../tabs/service';
+
 
 @Component({
   templateUrl: 'tabs.html'
 })
-export class TabsPage {
+export class TabsPage implements OnInit {
 
   tab1Root = HomePage;
   tab2Root = PreviewPage;
   tab3Root = CarPage;
   tab4Root = ContactPage;
 
-  constructor() {
+  tabIndex=0;
 
+  constructor(private srv: TabService) {
+
+  }
+
+  ngOnInit() {
+    this.tabIndex = this.srv.tabIndex;
   }
 }
