b3c373312b6a0f40306b73e7503abd5787305db0
tms <threemonoliths@gmail.com>
2019-04-24 17:40:34 +0800

    基本完成

--- room_reservation_admin/src/app/shared/shared.service.ts
+++ room_reservation_admin/src/app/shared/shared.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-export const baseUrl='http://localhost:4000/admin_api/v1/';
+export const baseUrl='http://112.132.249.108:4000/admin_api/v1/';
 
 @Injectable()
 export class GlobalService {
